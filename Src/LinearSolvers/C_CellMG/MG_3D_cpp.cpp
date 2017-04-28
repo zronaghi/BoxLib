08ebf6855e20df3c89ed0e6f1b62110549364522
Thorsten Kurth <thorstenkurth@me.com>
2017-04-28 16:33:29 -0700

    implemented GSRB

--- Src/LinearSolvers/C_CellMG/MG_3D_cpp.cpp
+++ Src/LinearSolvers/C_CellMG/MG_3D_cpp.cpp
@@ -4,6 +4,7 @@
 #include "CONSTANTS.H"
 #include "MG_F.H"
 #include <ArrayLim.H>
+#include <iostream>
 
 //ask Brian CONSTANTS
 //MultiGrid.cpp
@@ -33,11 +34,11 @@ const Real* f){
 	const int BL_k2Stride = BL_j2Stride * (2*(bx->length(1)) + 2*ng);
 	const int BL_n2Stride = BL_k2Stride * (2*(bx->length(2)) + 2*ng);
 	
-	const int *lo = bx->loVect();
-	const int *hi = bx->hiVect();
+	//const int *lo = bx->loVect();
+	//const int *hi = bx->hiVect();
 
 	//const int offset = 
-	int abs_i, abs_j, abs_k;
+	//int abs_i, abs_j, abs_k;
 
 	for (int n = 0; n<nc; n++){
 		for (int k = 0; k < bx->length(2); ++k) {
@@ -50,9 +51,9 @@ const Real* f){
 					i2 = 2*i;
 					//i2p1 = i2 + 1
 
-					abs_i = lo[0] + i;
-					abs_j = lo[1] + j;
-					abs_k = lo[2] + k;
+					//abs_i = lo[0] + i;
+					//abs_j = lo[1] + j;
+					//abs_k = lo[2] + k;
 
 					ijkn = (i + ng) + (j + ng)*BL_jStride + (k + ng)*BL_kStride + n*BL_nStride;
 
@@ -84,7 +85,6 @@ const Real* c){
  
 	//const Box* bx;
 	//Real *cp, *fp;
-    
 	int i2, j2, k2;
 	int ijkn;
 	int i2p1_j2p1_k2_n, i2_j2p1_k2_n, i2p1_j2_k2_n, i2_j2_k2_n;
@@ -100,11 +100,11 @@ const Real* c){
 	const int BL_k2Stride = BL_j2Stride * (2*(bx->length(1)) + 2*ng);
 	const int BL_n2Stride = BL_k2Stride * (2*(bx->length(2)) + 2*ng);
 	
-	const int *lo = bx->loVect();
-	const int *hi = bx->hiVect();
+	//const int *lo = bx->loVect();
+	//const int *hi = bx->hiVect();
 
 	//const int offset = 
-	int abs_i, abs_j, abs_k;
+	//int abs_i, abs_j, abs_k;
 
 	for (int n = 0; n<nc; n++){
 		for (int k = 0; k < bx->length(2); ++k) {
@@ -117,10 +117,11 @@ const Real* c){
 					i2 = 2*i;
 					//i2p1 = i2 + 1
 
-					abs_i = lo[0] + i;
-					abs_j = lo[1] + j;
-					abs_k = lo[2] + k;
-
+					//abs_i = lo[0] + i;
+					//abs_j = lo[1] + j;
+					//abs_k = lo[2] + k;
+					
+					//diagonal index
 					ijkn = (i + ng) + (j + ng)*BL_jStride + (k + ng)*BL_kStride + n*BL_nStride;
 
 					//add k2Stide, n2Stride, et
@@ -141,6 +142,122 @@ const Real* c){
 					f[i2_j2p1_k2p1_n]       += c[ijkn];
 					f[i2p1_j2_k2p1_n]       += c[ijkn];
 					f[i2_j2_k2p1_n]         += c[ijkn];
+				}
+			}
+		}
+	}
+}
+
+void C_GSRB_3D(
+const Box* bx,
+const Box* bbx,
+const int ng,
+const int nc,
+const int rb,
+const Real alpha,
+const Real beta,
+Real* phi,
+const Real* rhs,
+const Real* a,
+const Real* bX,
+const Real* bY,
+const Real* bZ,
+const Real* f0,
+const int* m0,
+const Real* f1,
+const int* m1,
+const Real* f2,
+const int* m2,
+const Real* f3,
+const int* m3,
+const Real* f4,
+const int* m4,
+const Real* f5,
+const int* m5,
+const Real* h)
+{
+
+	//field offsets
+	const int BL_jStride = bx->length(0) + 2*ng;
+	const int BL_kStride = BL_jStride * (bx->length(1) + 2*ng);
+	const int BL_nStride = BL_kStride * (bx->length(2) + 2*ng);
+
+	//box extends:
+	const int *lo = bx->loVect();
+	const int *hi = bx->hiVect();
+	//blo
+	const int *blo = bbx->loVect();
+	const int *bhi = bbx->hiVect();
+	
+	//some parameters
+	Real omega= 1.15;
+	Real dhx = beta/(h[0]*h[0]);
+	Real dhy = beta/(h[1]*h[1]);
+	Real dhz = beta/(h[2]*h[2]);
+	
+	//useful params:
+	int indf, indm;
+		
+	for (int n = 0; n<nc; n++){
+		for (int k = lo[2]; k <= hi[2]; ++k) {
+			for (int j = lo[1]; j <= hi[1]; ++j) {
+				//might need to revisit this in terms of offsets:
+				int ioff = (lo[0] + j + k + rb)%2;
+				for (int i = lo[0] + ioff; i <= hi[0]; i+=2) {
+					
+					//diagonal index
+					int i_j_k = (i + ng) + (j + ng)*BL_jStride + (k + ng)*BL_kStride;
+					int ip1_j_k = (i+1 + ng) + (j + ng)*BL_jStride + (k + ng)*BL_kStride;
+					int im1_j_k = (i-1 + ng) + (j + ng)*BL_jStride + (k + ng)*BL_kStride;
+					int i_jp1_k = (i + ng) + (j+1 + ng)*BL_jStride + (k + ng)*BL_kStride;
+					int i_jm1_k = (i + ng) + (j-1 + ng)*BL_jStride + (k + ng)*BL_kStride;
+					int i_j_kp1 = (i + ng) + (j + ng)*BL_jStride + (k+1 + ng)*BL_kStride;
+					int i_j_km1 = (i + ng) + (j + ng)*BL_jStride + (k-1 + ng)*BL_kStride;
+					
+					std::cout << i << " " << j << " " << k << std::endl;
+					
+					//deal with BC:
+					//cf0:
+					indf = (blo[0] + ng) + (j + ng)*BL_jStride + (k + ng)*BL_kStride;
+					indm = (blo[0]-1 + ng) + (j + ng)*BL_jStride + (k + ng)*BL_kStride;
+					Real cf0 = ( i==blo[0] && (m0[indm]>0) ? f0[indf] : 0. );
+					//cf1:
+					indf = (i + ng) + (blo[1] + ng)*BL_jStride + (k + ng)*BL_kStride;
+					indm = (i + ng) + (blo[1]-1 + ng)*BL_jStride + (k + ng)*BL_kStride;
+					Real cf1 = ( j==blo[1] && (m1[indm]>0) ? f1[indf] : 0. );
+					//cf2
+					indf = (i + ng) + (j + ng)*BL_jStride + (blo[2] + ng)*BL_kStride;
+					indm = (i + ng) + (j + ng)*BL_jStride + (blo[2]-1 + ng)*BL_kStride;
+					Real cf2 = ( k==blo[2] && (m2[indm]>0) ? f2[indf] : 0. );
+					//cf3
+					indf = (bhi[0] + ng) + (j + ng)*BL_jStride + (k + ng)*BL_kStride;
+					indm = (bhi[0]+1 + ng) + (j + ng)*BL_jStride + (k + ng)*BL_kStride;
+					Real cf3 = ( i==bhi[0] && (m3[indm]>0) ? f3[indf] : 0. );
+					//cf4
+					indf = (i + ng) + (bhi[1] + ng)*BL_jStride + (k + ng)*BL_kStride;
+					indm = (i + ng) + (bhi[1]+1 + ng)*BL_jStride + (k + ng)*BL_kStride;
+					Real cf4 = ( j==bhi[1] && (m4[indm]>0) ? f4[indf] : 0. );
+					//cf5
+					indf = (i + ng) + (j + ng)*BL_jStride + (bhi[2] + ng)*BL_kStride;
+					indm = (i + ng) + (j + ng)*BL_jStride + (bhi[2]+1 + ng)*BL_kStride;
+					Real cf5 = ( k==bhi[2] && (m5[indm]>0) ? f5[indf] : 0. );
+
+					double gamma = alpha * a[i_j_k] 
+									+ dhx * (bX[i_j_k] + bX[ip1_j_k])
+									+ dhy * (bY[i_j_k] + bY[i_jp1_k])
+									+ dhz * (bZ[i_j_k] + bZ[i_j_kp1]);
+					
+					double g_m_d = gamma
+									- dhx * (bX[i_j_k]*cf0 + bX[ip1_j_k]*cf3)
+									- dhy * (bY[i_j_k]*cf1 + bY[i_jp1_k]*cf4)
+									- dhz * (bZ[i_j_k]*cf2 + bZ[i_j_kp1]*cf5);
+					
+					double rho =  dhx * (bX[i_j_k]*phi[im1_j_k + n*BL_nStride] + bX[ip1_j_k]*phi[ip1_j_k + n*BL_nStride])
+								+ dhy * (bY[i_j_k]*phi[i_jm1_k + n*BL_nStride] + bY[i_jp1_k]*phi[i_jp1_k + n*BL_nStride])
+								+ dhz * (bZ[i_j_k]*phi[i_j_km1 + n*BL_nStride] + bZ[i_j_kp1]*phi[i_j_kp1 + n*BL_nStride]);
+					
+					double res = rhs[i_j_k + n*BL_nStride] - gamma * phi[i_j_k + n*BL_nStride] + rho;
+					phi[i_j_k + n*BL_nStride] += omega/g_m_d * res;
 				}
 			}
 		}
