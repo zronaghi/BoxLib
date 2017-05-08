8bc3a449cf342f8b8b1aadab4ca9dc7c6d21a44d
Thorsten Kurth <thorstenkurth@me.com>
2017-05-08 13:52:11 -0700

    starting port of other kernels

--- Src/LinearSolvers/C_CellMG/MG_3D_cpp.cpp
+++ Src/LinearSolvers/C_CellMG/MG_3D_cpp.cpp
@@ -67,7 +67,30 @@ const FArrayBox& c){
 }
 
 
-//GSRB kernel
+//-----------------------------------------------------------------------
+//      
+//     Gauss-Seidel Red-Black (GSRB):
+//     Apply the GSRB relaxation to the state phi for the equation
+//     L(phi) = alpha*a(x)*phi(x) - beta*Div(b(x)Grad(phi(x))) = rhs(x)
+//     central differenced, according to the arrays of boundary
+//     masks (m#) and auxiliary data (f#).
+//     
+//     In general, if the linear operator L=gamma*y-rho, the GS relaxation
+//     is y = (R - rho)/gamma.  Near a boundary, the ghost data is filled
+//     using a polynomial interpolant based on the "old" phi values, so
+//     L=(gamma-delta)*y - rho + delta*yOld.  The resulting iteration is
+//     
+//     y = (R - delta*yOld + rho)/(gamma - delta)
+//     
+//     This expression is valid additionally in the interior provided
+//     delta->0 there.  delta is constructed by summing all the
+//     contributions to the central stencil element coming from boundary 
+//     interpolants.  The f#s contain the corresponding coefficient of 
+//     the interpolating polynomial.  The masks are set > 0 if the boundary 
+//     value was filled with an interpolant involving the central stencil 
+//     element.
+//     
+//-----------------------------------------------------------------------
 void C_GSRB_3D(
 const Box& bx,
 const Box& bbx,
@@ -145,3 +168,117 @@ const Real* h)
 		}
 	}
 }
+
+////-----------------------------------------------------------------------
+////
+////     Fill in a matrix x vector operator here
+////
+//      subroutine C_ADOTX(
+//     $     y,DIMS(y),
+//     $     x,DIMS(x),
+//     $     alpha, beta,
+//     $     a, DIMS(a),
+//     $     bX,DIMS(bX),
+//     $     bY,DIMS(bY),
+//     $     bZ,DIMS(bZ),
+//     $     lo,hi,nc,
+//     $     h
+//     $     )
+//      implicit none
+//      REAL_T alpha, beta
+//      integer lo(BL_SPACEDIM), hi(BL_SPACEDIM), nc
+//      integer DIMDEC(y)
+//      integer DIMDEC(x)
+//      integer DIMDEC(a)
+//      integer DIMDEC(bX)
+//      integer DIMDEC(bY)
+//      integer DIMDEC(bZ)
+//      REAL_T  y(DIMV(y),nc)
+//      REAL_T  x(DIMV(x),nc)
+//      REAL_T  a(DIMV(a))
+//      REAL_T bX(DIMV(bX))
+//      REAL_T bY(DIMV(bY))
+//      REAL_T bZ(DIMV(bZ))
+//      REAL_T h(BL_SPACEDIM)
+//
+//      integer i,j,k,n
+//      REAL_T dhx,dhy,dhz
+//
+//      dhx = beta/h(1)**2
+//      dhy = beta/h(2)**2
+//      dhz = beta/h(3)**2
+//
+//      do n = 1, nc
+//         do k = lo(3), hi(3)
+//            do j = lo(2), hi(2)
+//               do i = lo(1), hi(1)
+//                  y(i,j,k,n) = alpha*a(i,j,k)*x(i,j,k,n)
+//     $                 - dhx*
+//     $                 (   bX(i+1,j,k)*( x(i+1,j,k,n) - x(i  ,j,k,n) )
+//     $                 -   bX(i  ,j,k)*( x(i  ,j,k,n) - x(i-1,j,k,n) ) )
+//     $                 - dhy*
+//     $                 (   bY(i,j+1,k)*( x(i,j+1,k,n) - x(i,j  ,k,n) )
+//     $                 -   bY(i,j  ,k)*( x(i,j  ,k,n) - x(i,j-1,k,n) ) )
+//     $                 - dhz*
+//     $                 (   bZ(i,j,k+1)*( x(i,j,k+1,n) - x(i,j,k  ,n) )
+//     $                 -   bZ(i,j,k  )*( x(i,j,k  ,n) - x(i,j,k-1,n) ) )
+//               end do
+//            end do
+//         end do
+//      end do
+//
+//      end
+//
+////-----------------------------------------------------------------------
+////
+////     Fill in a matrix x vector operator here
+////
+//      subroutine C_NORMA(
+//     &     res,
+//     $     alpha, beta,
+//     $     a, DIMS(a),
+//     $     bX,DIMS(bX),
+//     $     bY,DIMS(bY),
+//     $     bZ,DIMS(bZ),
+//     $     lo,hi,nc,
+//     $     h
+//     $     )
+//      implicit none
+//      REAL_T alpha, beta, res
+//      integer lo(BL_SPACEDIM), hi(BL_SPACEDIM), nc
+//      integer DIMDEC(a)
+//      integer DIMDEC(bX)
+//      integer DIMDEC(bY)
+//      integer DIMDEC(bZ)
+//      REAL_T  a(DIMV(a))
+//      REAL_T bX(DIMV(bX))
+//      REAL_T bY(DIMV(bY))
+//      REAL_T bZ(DIMV(bZ))
+//      REAL_T h(BL_SPACEDIM)
+//
+//      integer i,j,k,n
+//      REAL_T dhx,dhy,dhz
+//
+//      dhx = beta/h(1)**2
+//      dhy = beta/h(2)**2
+//      dhz = beta/h(3)**2
+//
+//      res = 0.0D0
+//
+//      do n = 1, nc
+//         do k = lo(3), hi(3)
+//            do j = lo(2), hi(2)
+//               do i = lo(1), hi(1)
+//                  res = max(res, abs(alpha*a(i,j,k)
+//     &                 + dhx*(bX(i+1,j,k) + bX(i,j,k))
+//     &                 + dhy*(bY(i,j+1,k) + bY(i,j,k))
+//     $                 + dhz*(bZ(i,j,k+1) + bZ(i,j,k)))
+//     &                 + abs( -dhx*bX(i+1,j,k)) + abs( -dhx*bX(i,j,k))
+//     &                 + abs( -dhy*bY(i,j+1,k)) + abs( -dhy*bY(i,j,k))
+//     &                 + abs( -dhz*bZ(i,j,k+1)) + abs( -dhz*bZ(i,j,k)))
+//               end do
+//            end do
+//         end do
+//      end do
+//
+//      end
