12bc733ec4a634393e2ed588f89f44cf71bb10dc
Thorsten Kurth <thorstenkurth@me.com>
2017-05-12 08:20:41 -0700

    changing kernels

--- Src/LinearSolvers/C_CellMG/MG_3D_cpp.cpp
+++ Src/LinearSolvers/C_CellMG/MG_3D_cpp.cpp
@@ -6,10 +6,6 @@
 #include <ArrayLim.H>
 #include <iostream>
 
-//forward declaration
-#pragma omp declare target
-IntVect IntVect::IntVect(int,int,int);
-
 //ask Brian CONSTANTS
 //MultiGrid.cpp
 
@@ -32,7 +28,8 @@ const FArrayBox& f){
 	const int* f_lo=c.loVect();
 	const int* f_hi=c.hiVect();
 	
-#pragma omp target data map(from: cpt) map(to: c_hi, c_lo, fpt, f_lo, f_hi, hi, lo)
+#pragma omp target data map(to: hi, lo, f_lo, f_hi, cpt, c_lo, c_hi)
+#pragma omp target update to(fpt)
 	{
 #pragma omp target teams distribute parallel for collapse(4) 
 		for (int n = 0; n<nc; n++){
@@ -46,6 +43,7 @@ const FArrayBox& f){
 			}
 		}
 	}
+#pragma omp target update from(cpt)
 }
 
 
@@ -67,8 +65,9 @@ const FArrayBox& c){
 	const Real* fpt=c.dataPtr();
 	const int* f_lo=c.loVect();
 	const int* f_hi=c.hiVect();
-	
-#pragma omp target data map(fpt) map(to: f_lo, f_hi, cpt, c_lo, c_hi, hi, lo)
+
+#pragma omp target data map(to: hi, lo, f_lo, f_hi, cpt, c_lo, c_hi)
+#pragma omp target update to(cpt)
 	{
 #pragma omp target teams distribute parallel for collapse(4) 
 		for (int n = 0; n<nc; n++){
@@ -88,6 +87,7 @@ const FArrayBox& c){
 			}
 		}
 	}
+#pragma omp target update from(fpt)
 }
 
 
@@ -149,14 +149,92 @@ const Real* h)
 	const int *blo = bbx.loVect();
 	const int *bhi = bbx.hiVect();
 	
+	//pointer, needed for c++ mapping
+	//phi:
+	Real* phipt=phi.dataPtr();
+	const int* phi_lo=phi.loVect();
+	const int* phi_hi=phi.hiVect();
+	//rhs
+	const Real* rhspt=rhs.dataPtr();
+	const int* rhs_lo=rhs.loVect();
+	const int* rhs_hi=rhs.hiVect();
+	//a
+	const Real* apt=a.dataPtr();
+	const int* a_lo=a.loVect();
+	const int* a_hi=a.hiVect();
+	//bX
+	const Real* bXpt=bX.dataPtr();
+	const int* bX_lo=bX.loVect();
+	const int* bX_hi=bX.hiVect();
+	//bY
+	const Real* bYpt=bY.dataPtr();
+	const int* bY_lo=bY.loVect();
+	const int* bY_hi=bY.hiVect();
+	//bZ
+	const Real* bZpt=bZ.dataPtr();
+	const int* bZ_lo=bZ.loVect();
+	const int* bZ_hi=bZ.hiVect();
+	//m0
+	const int* m0pt=m0.dataPtr();
+	const int* m0_lo=m0.loVect();
+	const int* m0_hi=m0.hiVect();
+	//m1
+	const int* m1pt=m1.dataPtr();
+	const int* m1_lo=m1.loVect();
+	const int* m1_hi=m1.hiVect();
+	//m2
+	const int* m2pt=m2.dataPtr();
+	const int* m2_lo=m2.loVect();
+	const int* m2_hi=m2.hiVect();
+	//m3
+	const int* m3pt=m3.dataPtr();
+	const int* m3_lo=m3.loVect();
+	const int* m3_hi=m3.hiVect();
+	//m4
+	const int* m4pt=m4.dataPtr();
+	const int* m4_lo=m4.loVect();
+	const int* m4_hi=m4.hiVect();
+	//m5
+	const int* m5pt=m5.dataPtr();
+	const int* m5_lo=m5.loVect();
+	const int* m5_hi=m5.hiVect();
+	//f0
+	const Real* f0pt=f0.dataPtr();
+	const int* f0_lo=f0.loVect();
+	const int* f0_hi=f0.hiVect();
+	//f1
+	const Real* f1pt=f1.dataPtr();
+	const int* f1_lo=f1.loVect();
+	const int* f1_hi=f1.hiVect();
+	//f2
+	const Real* f2pt=f2.dataPtr();
+	const int* f2_lo=f2.loVect();
+	const int* f2_hi=f2.hiVect();
+	//f3
+	const Real* f3pt=f3.dataPtr();
+	const int* f3_lo=f3.loVect();
+	const int* f3_hi=f3.hiVect();
+	//f4
+	const Real* f4pt=f4.dataPtr();
+	const int* f4_lo=f4.loVect();
+	const int* f4_hi=f4.hiVect();
+	//f5
+	const Real* f5pt=f5.dataPtr();
+	const int* f5_lo=f5.loVect();
+	const int* f5_hi=f5.hiVect();
+	
 	//some parameters
 	Real omega= 1.15;
 	Real dhx = beta/(h[0]*h[0]);
 	Real dhy = beta/(h[1]*h[1]);
 	Real dhz = beta/(h[2]*h[2]);
 	
-#pragma omp target data map(tofrom:phi) map(to:rhs,m0,m1,m2,m3,m4,m5,f0,f1,f2,f3,f4,f5,a,bX,bY,bZ, blo,bhi,lo,hi)
-#pragma omp target update to(rhs,phi,bX,bY,bZ,a,m0,m1,m2,m3,m4,m5,f0,f1,f2,f3,f4,f5, blo,bhi,lo,hi)
+#pragma omp target data map(to: blo, bhi, lo, hi)
+#pragma omp target data map(to: phi_lo, phi_hi, rhs_lo, rhs_hi, a_lo, a_hi)
+#pragma omp target data map(to: bX_lo, bX_hi, bY_lo, bY_hi, bZ_lo, bZ_hi)
+#pragma omp target data map(to: m0_lo, m0_hi, m1_lo, m1_hi, m2_lo, m2_hi, m3_lo, m3_hi, m4_lo, m4_hi, m5_lo, m5_hi)
+#pragma omp target data map(to: f0_lo, f0_hi, f1_lo, f1_hi, f2_lo, f2_hi, f3_lo, f3_hi, f4_lo, f4_hi, f5_lo, f5_hi) 
+#pragma omp target update to(phipt,rhspt,apt,bXpt,bYpt,bZpt,m0pt,m1pt,m2pt,m3pt,m4pt,m5pt,f0pt,f1pt,f2pt,f3pt,f4pt,f5pt)
 	{
 #pragma omp target teams distribute collapse(3)
 		for (int n = 0; n<nc; n++){
@@ -196,7 +274,7 @@ const Real* h)
 			}
 		}
 	}
-#pragma omp target update from(phi)
+#pragma omp target update from(phipt)
 }
 
 //-----------------------------------------------------------------------
@@ -221,13 +299,41 @@ const Real* h)
 	const int *lo = bx.loVect();
 	const int *hi = bx.hiVect();
 	
+	//pointer, needed for c++ mapping
+	//y
+	Real* ypt=y.dataPtr();
+	const int* y_lo=y.loVect();
+	const int* y_hi=y.hiVect();
+	//x
+	const Real* xpt=x.dataPtr();
+	const int* x_lo=x.loVect();
+	const int* x_hi=x.hiVect();
+	//a
+	const Real* apt=a.dataPtr();
+	const int* a_lo=a.loVect();
+	const int* a_hi=a.hiVect();
+	//bX
+	const Real* bXpt=bX.dataPtr();
+	const int* bX_lo=bX.loVect();
+	const int* bX_hi=bX.hiVect();
+	//bY
+	const Real* bYpt=bY.dataPtr();
+	const int* bY_lo=bY.loVect();
+	const int* bY_hi=bY.hiVect();
+	//bZ
+	const Real* bZpt=bZ.dataPtr();
+	const int* bZ_lo=bZ.loVect();
+	const int* bZ_hi=bZ.hiVect();
+	
 	//some parameters
 	Real dhx = beta/(h[0]*h[0]);
 	Real dhy = beta/(h[1]*h[1]);
 	Real dhz = beta/(h[2]*h[2]);
 
-#pragma omp target data map(from: y) map(to: a, bX,bY,bZ, x, lo, hi)
-#pragma omp target update to(a,x)
+#pragma omp target data map(to: lo, hi)
+#pragma omp target data map(to: y_lo, y_hi, x_lo, x_hi, a_lo, a_hi)
+#pragma omp target data map(to: bX_lo, bX_hi, bY_lo, bY_hi, bZ_lo, bZ_hi)
+#pragma omp target update to(apt,xpt,bXpt,bYpt,bZpt)
 	{
 #pragma omp target teams distribute parallel for collapse(4)
 		for (int n = 0; n<nc; n++){
@@ -249,7 +355,7 @@ const Real* h)
 			}
 		}
 	}
-#pragma omp target update from(y)
+#pragma omp target update from(ypt)
 }
 
 //-----------------------------------------------------------------------
@@ -273,6 +379,24 @@ const Real* h)
 	const int *lo = bx.loVect();
 	const int *hi = bx.hiVect();
 	
+	//pointer, needed for c++ mapping
+	//a
+	const Real* apt=a.dataPtr();
+	const int* a_lo=a.loVect();
+	const int* a_hi=a.hiVect();
+	//bX
+	const Real* bXpt=bX.dataPtr();
+	const int* bX_lo=bX.loVect();
+	const int* bX_hi=bX.hiVect();
+	//bY
+	const Real* bYpt=bY.dataPtr();
+	const int* bY_lo=bY.loVect();
+	const int* bY_hi=bY.hiVect();
+	//bZ
+	const Real* bZpt=bZ.dataPtr();
+	const int* bZ_lo=bZ.loVect();
+	const int* bZ_hi=bZ.hiVect();
+	
 	//some parameters
 	Real dhx = beta/(h[0]*h[0]);
 	Real dhy = beta/(h[1]*h[1]);
@@ -281,10 +405,12 @@ const Real* h)
 	//initialize to zero
     res = 0.0;
 
-#pragma omp target data map(to: a,bX,bY,bZ, lo, hi) map(tofrom:res)
-#pragma omp target update to(a,bX,bY,bZ)
+#pragma omp target data map(to: lo, hi)
+#pragma omp target data map(to: a_lo, a_hi)
+#pragma omp target data map(to: bX_lo, bX_hi, bY_lo, bY_hi, bZ_lo, bZ_hi)
+#pragma omp target update to(apt,bXpt,bYpt,bZpt)
 	{
-#pragma omp teams distribute parallel for collapse(4) firstprivate(alpha,dhx,dhy,dhz) private(k,j,i) reduction(max:res)
+#pragma omp teams distribute parallel for collapse(4) firstprivate(alpha,dhx,dhy,dhz) reduction(max:res)
 		for (int n = 0; n<nc; n++){
 			for (int k = lo[2]; k <= hi[2]; ++k) {
 				for (int j = lo[1]; j <= hi[1]; ++j) {
