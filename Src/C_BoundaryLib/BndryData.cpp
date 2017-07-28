05f02b655f4fcc80d993094f9643b3283deeb7ab
Thorsten Kurth <thorstenkurth@me.com>
2017-07-28 15:34:49 -0700

    fixing bugs

--- Src/C_BoundaryLib/BndryData.cpp
+++ Src/C_BoundaryLib/BndryData.cpp
@@ -13,39 +13,39 @@ int BndryData::NTangHalfWidth = 5;  // ref_ratio + 1, so won't work if ref_ratio
 
 BndryData::BndryData ()
     :
-    m_ncomp(-1), m_defined(false) {}
+m_ncomp(-1), m_defined(false) {}
 
 BndryData::BndryData (const BoxArray& _grids,
-                      int             _ncomp, 
-                      const Geometry& _geom,
-		      ParallelDescriptor::Color color)
+int             _ncomp, 
+const Geometry& _geom,
+ParallelDescriptor::Color color)
     :
-    geom(_geom),
-    m_ncomp(_ncomp),
-    m_defined(false)
+geom(_geom),
+m_ncomp(_ncomp),
+m_defined(false)
 {
     define(_grids,_ncomp,_geom,color);
 }
 
 void
-BndryData::setBoundCond (Orientation     _face,
-                         int              _n,
-                         int              _comp,
-                         const BoundCond& _bcn)
+    BndryData::setBoundCond (Orientation     _face,
+int              _n,
+int              _comp,
+const BoundCond& _bcn)
 {
     bcond[_n][_face][_comp] = _bcn;
 }
 
 void
-BndryData::setBoundLoc (Orientation _face,
-                        int         _n,
-                        Real        _val)
+    BndryData::setBoundLoc (Orientation _face,
+int         _n,
+Real        _val)
 {
     bcloc[_n][_face] = _val;
 }
 
 const Array< Array<BoundCond> >&
-BndryData::bndryConds (int igrid) const
+    BndryData::bndryConds (int igrid) const
 {
     std::map< int, Array< Array<BoundCond> > >::const_iterator it = bcond.find(igrid);
     BL_ASSERT(it != bcond.end());
@@ -53,7 +53,7 @@ BndryData::bndryConds (int igrid) const
 }
 
 const BndryData::RealTuple&
-BndryData::bndryLocs (int igrid) const
+    BndryData::bndryLocs (int igrid) const
 {
     std::map<int,RealTuple>::const_iterator it = bcloc.find(igrid);
     BL_ASSERT(it != bcloc.end());
@@ -61,7 +61,7 @@ BndryData::bndryLocs (int igrid) const
 }
 
 void
-BndryData::init (const BndryData& src)
+    BndryData::init (const BndryData& src)
 {
     geom      = src.geom;
     m_ncomp   = src.m_ncomp;
@@ -73,29 +73,29 @@ BndryData::init (const BndryData& src)
     masks.resize(2*BL_SPACEDIM, PArrayManage);
     for (int i = 0; i < 2*BL_SPACEDIM; i++)
     {
-	const MultiMask& smasks = src.masks[i];
-	masks.set(i, new MultiMask(smasks.boxArray(), smasks.DistributionMap(), smasks.nComp()));
-	MultiMask::Copy(masks[i], smasks);
+        const MultiMask& smasks = src.masks[i];
+        masks.set(i, new MultiMask(smasks.boxArray(), smasks.DistributionMap(), smasks.nComp()));
+        MultiMask::Copy(masks[i], smasks);
     }
 }
 
 BndryData::BndryData (const BndryData& src)
     :
-    BndryRegister(src),
-    m_ncomp(src.m_ncomp)
+BndryRegister(src),
+m_ncomp(src.m_ncomp)
 {
     init(src);
 }
 
 BndryData&
-BndryData::operator= (const BndryData& src)
+    BndryData::operator= (const BndryData& src)
 {
     if (this != &src)
     {
         BndryRegister::operator=(src);
-	for (int i = 0; i < 2*BL_SPACEDIM; i++) {
-	    bndry[i].clear();
-	}
+        for (int i = 0; i < 2*BL_SPACEDIM; i++) {
+            bndry[i].clear();
+        }
         init(src);
     }
     return *this;
@@ -106,10 +106,10 @@ BndryData::~BndryData ()
 }
 
 void
-BndryData::define (const BoxArray& _grids,
-                   int             _ncomp,
-                   const Geometry& _geom,
-		   ParallelDescriptor::Color color)
+    BndryData::define (const BoxArray& _grids,
+int             _ncomp,
+const Geometry& _geom,
+ParallelDescriptor::Color color)
 {
     BL_PROFILE("BndryData::define()");
 
@@ -127,7 +127,7 @@ BndryData::define (const BoxArray& _grids,
     }
     geom    = _geom;
     m_ncomp = _ncomp;
-
+    
     BndryRegister::setBoxes(_grids);
 
     masks.clear();
@@ -136,12 +136,12 @@ BndryData::define (const BoxArray& _grids,
     for (OrientationIter fi; fi; ++fi)
     {
         Orientation face = fi();
-
+        
         BndryRegister::define(face,IndexType::TheCellType(),0,1,1,_ncomp,color);
-	
-	masks.set(face, new MultiMask(grids, bndry[face].DistributionMap(), geom,
-				      face, 0, 2, NTangHalfWidth, 1, true));
+        
+        masks.set(face, new MultiMask(grids, bndry[face].DistributionMap(), geom, face, 0, 2, NTangHalfWidth, 1, true));
     }
+    
     //
     // Define "bcond" and "bcloc".
     //
@@ -150,8 +150,8 @@ BndryData::define (const BoxArray& _grids,
     //
     //
     for (FabSetIter bfsi(bndry[Orientation(0,Orientation::low)]);
-         bfsi.isValid();
-         ++bfsi)
+    bfsi.isValid();
+    ++bfsi)
     {
         const int idx = bfsi.index();
         //
