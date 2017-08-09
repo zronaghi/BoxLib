7176a163e4bbfb40dd0f78b05b991968cf566bc4
Thorsten Kurth <thorstenkurth@me.com>
2017-08-09 13:00:20 -0400

    debugging kokkosy function

--- Src/LinearSolvers/C_CellMG/MG_3D_cpp.cpp
+++ Src/LinearSolvers/C_CellMG/MG_3D_cpp.cpp
@@ -181,26 +181,6 @@ private:
 };
 
 
-//Average Functor:
-struct C_AVERAGE_Functor {
-    // Data used by the loop body
-    FArrayBox* c;
-    FArrayBox* f;
-  
-    // Constructor to initialize the data
-    C_AVERAGE_Functor(FArrayBox* c_, const FArrayBox* f_){
-        c=c_;
-        f=const_cast<FArrayBox*>(f_);
-    }
-
-    // Loop body as an operator
-    KOKKOS_INLINE_FUNCTION
-    void operator() (const int& n, const int& k, const int& j, const int& i) const {
-        (*c)(IntVect(i,j,k),n) =  ((*f)(IntVect(2*i+1,2*j+1,2*k),n) + (*f)(IntVect(2*i,2*j+1,2*k),n) + (*f)(IntVect(2*i+1,2*j,2*k),n) + (*f)(IntVect(2*i,2*j,2*k),n))*0.125;
-        (*c)(IntVect(i,j,k),n) += ((*f)(IntVect(2*i+1,2*j+1,2*k+1),n) + (*f)(IntVect(2*i,2*j+1,2*k+1),n) + (*f)(IntVect(2*i+1,2*j,2*k+1),n) + (*f)(IntVect(2*i,2*j,2*k+1),n))*0.125;
-    }
-};
-
 //Average Kernel
 void C_AVERAGE(
     const Box& bx,
@@ -210,41 +190,27 @@ const FArrayBox& f){
 	
     const int *lo = bx.loVect();
     const int *hi = bx.hiVect();
+    const int* cb = bx.cbVect();
 	
-    typedef Kokkos::Experimental::MDRangePolicy<Kokkos::Experimental::Rank<4,Kokkos::Experimental::Iterate::Right,Kokkos::Experimental::Iterate::Right>> t_policy;
-    // Create a functor
-    C_AVERAGE_Functor ave_functor(&c,&f);
-    // Execute functor
-    Kokkos::Experimental::md_parallel_for(t_policy({0,lo[2],lo[1],lo[0]},{nc,hi[2]+1,hi[1]+1,hi[0]+1},{1,4,4,1024000}),ave_functor);
+    //create views
+    ViewFab<Real> cv(c,"c"), fv(f,"f");
+    
+    //define policy
+    typedef Kokkos::Experimental::MDRangePolicy<Kokkos::Experimental::Rank<3> > t_policy;
+
+    //execute
+    for(int n=0; n<nc; n++){
+      Kokkos::Experimental::md_parallel_for(t_policy({lo[2],lo[1],lo[0]},{hi[2]+1,hi[1]+1,hi[0]+1},{cb[2],cb[1],cb[0]}),
+					    [&](const int& k, const int& j, const int& i){
+					      cv(i,j,k,n) =  (fv(2*i+1,2*j+1,2*k,n) + fv(2*i,2*j+1,2*k,n) + fv(2*i+1,2*j,2*k,n) + fv(2*i,2*j,2*k,n))*0.125;
+					      cv(i,j,k,n) += (fv(2*i+1,2*j+1,2*k+1,n) + fv(2*i,2*j+1,2*k+1,n) + fv(2*i+1,2*j,2*k+1,n) + fv(2*i,2*j,2*k+1,n))*0.125;
+					    });
+	}
+    //write back
+    cv.fill(c);
+
 }
 
-
-
-//Interpolation Functor:
-struct C_INTERP_Functor {
-    // Data used by the loop body
-    FArrayBox* f;
-    FArrayBox* c;
-  
-    // Constructor to initialize the data
-    C_INTERP_Functor(FArrayBox* f_, const FArrayBox* c_){
-        f=f_;
-        c=const_cast<FArrayBox*>(c_);
-    }
-
-    // Loop body as an operator
-    KOKKOS_INLINE_FUNCTION
-    void operator() (const int& n, const int& k, const int& j, const int& i) const {
-        (*f)(IntVect(2*i+1,2*j+1,2*k  ),n)       += (*c)(IntVect(i,j,k),n);
-        (*f)(IntVect(2*i  ,2*j+1,2*k  ),n)       += (*c)(IntVect(i,j,k),n);
-        (*f)(IntVect(2*i+1,2*j  ,2*k  ),n)       += (*c)(IntVect(i,j,k),n);
-        (*f)(IntVect(2*i  ,2*j  ,2*k  ),n)       += (*c)(IntVect(i,j,k),n);
-        (*f)(IntVect(2*i+1,2*j+1,2*k+1),n)       += (*c)(IntVect(i,j,k),n);
-        (*f)(IntVect(2*i  ,2*j+1,2*k+1),n)       += (*c)(IntVect(i,j,k),n);
-        (*f)(IntVect(2*i+1,2*j  ,2*k+1),n)       += (*c)(IntVect(i,j,k),n);
-        (*f)(IntVect(2*i  ,2*j  ,2*k+1),n)       += (*c)(IntVect(i,j,k),n);
-    }
-};
 
 //Interpolation Kernel
 void C_INTERP(const Box& bx,
@@ -252,14 +218,32 @@ const int nc,
 FArrayBox& f,
 const FArrayBox& c){
 	
-    const int *lo = bx.loVect();
-    const int *hi = bx.hiVect();
+  const int *lo = bx.loVect();
+  const int *hi = bx.hiVect();
+  const int* cb = bx.cbVect();
+
+  //create views                                                                                                                                                                                                  
+  ViewFab<Real> cv(c,"c"), fv(f,"f");
+
+  //define policy                                                                                                                                                                                                 
+  typedef Kokkos::Experimental::MDRangePolicy<Kokkos::Experimental::Rank<3> > t_policy;
 	
-    typedef Kokkos::Experimental::MDRangePolicy<Kokkos::Experimental::Rank<4,Kokkos::Experimental::Iterate::Right,Kokkos::Experimental::Iterate::Right>> t_policy;
-    // Create a functor
-    C_INTERP_Functor int_functor(&f,&c);
-    // Execute functor
-    Kokkos::Experimental::md_parallel_for(t_policy({0,lo[2],lo[1],lo[0]},{nc,hi[2]+1,hi[1]+1,hi[0]+1},{1,4,4,1024000}),int_functor);
+  // Execute functor
+  for(int n=0; n<nc; n++){
+    Kokkos::Experimental::md_parallel_for(t_policy({lo[2],lo[1],lo[0]},{hi[2]+1,hi[1]+1,hi[0]+1},{cb[2],cb[1],cb[0]}),
+					  [&](const int& k, const int& j, const int& i){
+					    fv(2*i+1,2*j+1,2*k  ,n)       += cv(i,j,k,n);
+					    fv(2*i  ,2*j+1,2*k  ,n)       += cv(i,j,k,n);
+					    fv(2*i+1,2*j  ,2*k  ,n)       += cv(i,j,k,n);
+					    fv(2*i  ,2*j  ,2*k  ,n)       += cv(i,j,k,n);
+					    fv(2*i+1,2*j+1,2*k+1,n)       += cv(i,j,k,n);
+					    fv(2*i  ,2*j+1,2*k+1,n)       += cv(i,j,k,n);
+					    fv(2*i+1,2*j  ,2*k+1,n)       += cv(i,j,k,n);
+					    fv(2*i  ,2*j  ,2*k+1,n)       += cv(i,j,k,n);
+					  });
+  }
+  fv.fill(f);
+  
 }
 
 
@@ -287,234 +271,6 @@ const FArrayBox& c){
 //     element.
 //     
 //-----------------------------------------------------------------------
-#if 0
-//GSRB Functor
-struct C_GSRB_Functor {
-    // Data used by the loop body
-    int rb, lo, hi, *blo, *bhi;
-    Real alpha, beta;
-    FArrayBox *phi, *rhs, *a, *bX, *bY, *bZ;
-    FArrayBox *f0, *f1, *f2, *f3, *f4, *f5;
-    Mask *m0, *m1, *m2, *m3, *m4, *m5;
-    Real* h;
-    Real omega, dhx, dhy, dhz;
-  
-    // Constructor to initialize the data
-    C_GSRB_Functor(
-    const int rb_,
-    const int lo_,
-    const int hi_,
-    const Real alpha_,
-    const Real beta_,
-    FArrayBox* phi_,
-    const FArrayBox* rhs_,
-    const FArrayBox* a_,
-    const FArrayBox* bX_,
-    const FArrayBox* bY_,
-    const FArrayBox* bZ_,
-    const FArrayBox* f0_,
-    const Mask* m0_,
-    const FArrayBox* f1_,
-    const Mask* m1_,
-    const FArrayBox* f2_,
-    const Mask* m2_,
-    const FArrayBox* f3_,
-    const Mask* m3_,
-    const FArrayBox* f4_,
-    const Mask* m4_,
-    const FArrayBox* f5_,
-    const Mask* m5_,
-    const Real* h_,
-    const int* blo_,
-    const int* bhi_) : rb(rb_), lo(lo_), hi(hi_), alpha(alpha_), beta(beta_)
-    {
-		
-        phi=phi_;
-        rhs=const_cast<FArrayBox*>(rhs_);
-        a=const_cast<FArrayBox*>(a_);
-        bX=const_cast<FArrayBox*>(bX_);
-        bY=const_cast<FArrayBox*>(bY_);
-        bZ=const_cast<FArrayBox*>(bZ_);
-        f0=const_cast<FArrayBox*>(f0_);
-        f1=const_cast<FArrayBox*>(f1_);
-        f2=const_cast<FArrayBox*>(f2_);
-        f3=const_cast<FArrayBox*>(f3_);
-        f4=const_cast<FArrayBox*>(f4_);
-        f5=const_cast<FArrayBox*>(f5_);
-        m0=const_cast<Mask*>(m0_);
-        m1=const_cast<Mask*>(m1_);
-        m2=const_cast<Mask*>(m2_);
-        m3=const_cast<Mask*>(m3_);
-        m4=const_cast<Mask*>(m4_);
-        m5=const_cast<Mask*>(m5_);
-        h=const_cast<Real*>(h_);
-        blo=const_cast<int*>(blo_);
-        bhi=const_cast<int*>(bhi_);
-        
-        //some parameters
-        omega= 1.15;
-        dhx = beta/(h[0]*h[0]);
-        dhy = beta/(h[1]*h[1]);
-        dhz = beta/(h[2]*h[2]);
-    }
-
-    // Loop body as an operator
-    KOKKOS_INLINE_FUNCTION
-    void operator() (const int& n, const int& k, const int& j) const {
-        int ioff = (lo + j + k + rb)%2;
-        for (int i = lo + ioff; i <= hi; i+=2) {
-        
-            //BC terms
-            Real cf0 = ( (i==blo[0]) && ((*m0)(IntVect(blo[0]-1,j,k))>0) ? (*f0)(IntVect(blo[0],j,k)) : 0. );
-            Real cf1 = ( (j==blo[1]) && ((*m1)(IntVect(i,blo[1]-1,k))>0) ? (*f1)(IntVect(i,blo[1],k)) : 0. );
-            Real cf2 = ( (k==blo[2]) && ((*m2)(IntVect(i,j,blo[2]-1))>0) ? (*f2)(IntVect(i,j,blo[2])) : 0. );
-            Real cf3 = ( (i==bhi[0]) && ((*m3)(IntVect(bhi[0]+1,j,k))>0) ? (*f3)(IntVect(bhi[0],j,k)) : 0. );
-            Real cf4 = ( (j==bhi[1]) && ((*m4)(IntVect(i,bhi[1]+1,k))>0) ? (*f4)(IntVect(i,bhi[1],k)) : 0. );
-            Real cf5 = ( (k==bhi[2]) && ((*m5)(IntVect(i,j,bhi[2]+1))>0) ? (*f5)(IntVect(i,j,bhi[2])) : 0. );
-		
-            //assign ORA constants
-            double gamma = alpha * (*a)(IntVect(i,j,k))
-                        + dhx * ((*bX)(IntVect(i,j,k)) + (*bX)(IntVect(i+1,j,k)))
-                        + dhy * ((*bY)(IntVect(i,j,k)) + (*bY)(IntVect(i,j+1,k)))
-                        + dhz * ((*bZ)(IntVect(i,j,k)) + (*bZ)(IntVect(i,j,k+1)));
-		
-            double g_m_d = gamma
-                        - dhx * ((*bX)(IntVect(i,j,k))*cf0 + (*bX)(IntVect(i+1,j,k))*cf3)
-                        - dhy * ((*bY)(IntVect(i,j,k))*cf1 + (*bY)(IntVect(i,j+1,k))*cf4)
-                        - dhz * ((*bZ)(IntVect(i,j,k))*cf2 + (*bZ)(IntVect(i,j,k+1))*cf5);
-		
-            double rho =  dhx * ((*bX)(IntVect(i,j,k))*(*phi)(IntVect(i-1,j,k),n) + (*bX)(IntVect(i+1,j,k))*(*phi)(IntVect(i+1,j,k),n))
-                        + dhy * ((*bY)(IntVect(i,j,k))*(*phi)(IntVect(i,j-1,k),n) + (*bY)(IntVect(i,j+1,k))*(*phi)(IntVect(i,j+1,k),n))
-                        + dhz * ((*bZ)(IntVect(i,j,k))*(*phi)(IntVect(i,j,k-1),n) + (*bZ)(IntVect(i,j,k+1))*(*phi)(IntVect(i,j,k+1),n));
-		
-            double res = (*rhs)(IntVect(i,j,k),n) - gamma * (*phi)(IntVect(i,j,k),n) + rho;
-            (*phi)(IntVect(i,j,k),n) += omega/g_m_d * res;
-        }
-    }
-};
-
-#else
-
-//GSRB Functor
-struct C_GSRB_View_Functor {
-    // Data used by the loop body
-    int rb, comp, *lo, *hi, *blo, *bhi;
-    Real alpha, beta;
-    ViewFab<Real> phi, rhs, a, bX, bY, bZ, f0, f1, f2, f3, f4, f5;
-    ViewFab<int> m0, m1, m2, m3, m4, m5;
-    Real* h;
-    Real omega, dhx, dhy, dhz;
-  
-    // Constructor to initialize the data
-    C_GSRB_View_Functor(
-    const int rb_,
-    const Real alpha_,
-    const Real beta_,
-    ViewFab<Real> phi_,
-    ViewFab<Real> rhs_,
-    ViewFab<Real> a_,
-    ViewFab<Real> bX_,
-    ViewFab<Real> bY_,
-    ViewFab<Real> bZ_,
-    ViewFab<Real> f0_,
-    ViewFab<int> m0_,
-    ViewFab<Real> f1_,
-    ViewFab<int> m1_,
-    ViewFab<Real> f2_,
-    ViewFab<int> m2_,
-    ViewFab<Real> f3_,
-    ViewFab<int> m3_,
-    ViewFab<Real> f4_,
-    ViewFab<int> m4_,
-    ViewFab<Real> f5_,
-    ViewFab<int> m5_,
-    const Real* h_,
-    const int* lo_,
-    const int* hi_,
-    const int* blo_,
-    const int* bhi_) : rb(rb_), alpha(alpha_), beta(beta_), comp(0)
-    {
-        phi=phi_;
-        rhs=rhs_;
-        a=a_;
-        bX=bX_;
-        bY=bY_;
-        bZ=bZ_;
-        f0=f0_;
-        f1=f1_;
-        f2=f2_;
-        f3=f3_;
-        f4=f4_;
-        f5=f5_;
-        m0=m0_;
-        m1=m1_;
-        m2=m2_;
-        m3=m3_;
-        m4=m4_;
-        m5=m5_;
-        h=const_cast<Real*>(h_);
-        lo=const_cast<int*>(lo_);
-        hi=const_cast<int*>(hi_);
-        blo=const_cast<int*>(blo_);
-        bhi=const_cast<int*>(bhi_);
-        
-        //some parameters
-        omega= 1.15;
-        dhx = beta/(h[0]*h[0]);
-        dhy = beta/(h[1]*h[1]);
-        dhz = beta/(h[2]*h[2]);
-    }
-    
-    void set_comp(const int& comp_){
-        comp=comp_;
-    }
-    
-    void print_rhs(){
-        for(unsigned int k=lo[2]; k<=hi[2]; k++){
-            for(unsigned int j=lo[1]; j<=hi[1]; j++){
-                for(unsigned int i=lo[0]; i<=hi[0]; i++){
-                    std::cout << i << "," << j << "," << k << ": " << rhs(i,j,k,comp) << std::endl;
-                }
-            }
-        }
-    }
-
-    //// Loop body as an operator
-    KOKKOS_INLINE_FUNCTION
-    Real operator() (const int& i, const int& j, const int& k) const {
-        if( (i + j + k + rb) % 2 != 0 ){
-        
-            //BC terms
-            Real cf0 = ( (i==blo[0]) && (m0(blo[0]-1,j,k)>0) ? f0(blo[0],j,k) : 0. );
-            Real cf1 = ( (j==blo[1]) && (m1(i,blo[1]-1,k)>0) ? f1(i,blo[1],k) : 0. );
-            Real cf2 = ( (k==blo[2]) && (m2(i,j,blo[2]-1)>0) ? f2(i,j,blo[2]) : 0. );
-            Real cf3 = ( (i==bhi[0]) && (m3(bhi[0]+1,j,k)>0) ? f3(bhi[0],j,k) : 0. );
-            Real cf4 = ( (j==bhi[1]) && (m4(i,bhi[1]+1,k)>0) ? f4(i,bhi[1],k) : 0. );
-            Real cf5 = ( (k==bhi[2]) && (m5(i,j,bhi[2]+1)>0) ? f5(i,j,bhi[2]) : 0. );
-		
-            //assign ORA constants
-            double gamma = alpha * a(i,j,k)
-                        + dhx * (bX(i,j,k) + bX(i+1,j,k))
-                        + dhy * (bY(i,j,k) + bY(i,j+1,k))
-                        + dhz * (bZ(i,j,k) + bZ(i,j,k+1));
-		
-            double g_m_d = gamma
-                        - dhx * (bX(i,j,k)*cf0 + bX(i+1,j,k)*cf3)
-                        - dhy * (bY(i,j,k)*cf1 + bY(i,j+1,k)*cf4)
-                        - dhz * (bZ(i,j,k)*cf2 + bZ(i,j,k+1)*cf5);
-		
-            double rho =  dhx * (bX(i,j,k)*phi(i-1,j,k,comp) + bX(i+1,j,k)*phi(i+1,j,k,comp))
-                        + dhy * (bY(i,j,k)*phi(i,j-1,k,comp) + bY(i,j+1,k)*phi(i,j+1,k,comp))
-                        + dhz * (bZ(i,j,k)*phi(i,j,k-1,comp) + bZ(i,j,k+1)*phi(i,j,k+1,comp));
-		
-            double res = rhs(i,j,k,comp) - gamma * phi(i,j,k,comp) + rho;
-            return omega/g_m_d * res;
-        }
-        else return 0.;
-    }
-};
-#endif
-
 //GSRB kernel
 void C_GSRB_3D(
     const Box& bx,
@@ -660,45 +416,6 @@ const Real* h)
 //
 //     Fill in a matrix x vector operator here
 //
-//adotx functor
-struct C_ADOTX_Functor {
-    // Data used by the loop body
-    FArrayBox *y, *x, *a, *bX, *bY, *bZ;
-    Real* h;
-    Real alpha, beta, dhx, dhy, dhz;
-  
-    // Constructor to initialize the data
-    C_ADOTX_Functor(Real alpha_, Real beta_, FArrayBox* y_, const FArrayBox* x_, const FArrayBox* a_, const FArrayBox* bX_, const FArrayBox* bY_, const FArrayBox* bZ_, const Real* h_) : alpha(alpha_), beta(beta_){
-        y=y_;
-        x=const_cast<FArrayBox*>(x_);
-        a=const_cast<FArrayBox*>(a_);
-        bX=const_cast<FArrayBox*>(bX_);
-        bY=const_cast<FArrayBox*>(bY_);
-        bZ=const_cast<FArrayBox*>(bZ_);
-        h=const_cast<Real*>(h_);
-        
-        //some parameters
-        dhx = beta/(h[0]*h[0]);
-        dhy = beta/(h[1]*h[1]);
-        dhz = beta/(h[2]*h[2]);
-    }
-
-    // Loop body as an operator
-    KOKKOS_INLINE_FUNCTION
-    void operator() (const int& n, const int& k, const int& j, const int& i) const {
-        (*y)(IntVect(i,j,k),n) = alpha*(*a)(IntVect(i,j,k))*(*x)(IntVect(i,j,k),n)
-                            - dhx * (   (*bX)(IntVect(i+1,j,  k  )) * ( (*x)(IntVect(i+1,j,  k),  n) - (*x)(IntVect(i,  j,  k  ),n) )
-                                      - (*bX)(IntVect(i,  j,  k  )) * ( (*x)(IntVect(i,  j,  k),  n) - (*x)(IntVect(i-1,j,  k  ),n) ) 
-                                    )
-                            - dhy * (   (*bY)(IntVect(i,  j+1,k  )) * ( (*x)(IntVect(i,  j+1,k),  n) - (*x)(IntVect(i,  j  ,k  ),n) )
-                                      - (*bY)(IntVect(i,  j,  k  )) * ( (*x)(IntVect(i,  j,  k),  n) - (*x)(IntVect(i,  j-1,k  ),n) )
-                                    )
-                            - dhz * (   (*bZ)(IntVect(i,  j,  k+1)) * ( (*x)(IntVect(i,  j,  k+1),n) - (*x)(IntVect(i,  j  ,k  ),n) )
-                                      - (*bZ)(IntVect(i,  j,  k  )) * ( (*x)(IntVect(i,  j,  k),  n) - (*x)(IntVect(i,  j,  k-1),n) )
-                                    );
-    }
-};
-
 void C_ADOTX(
     const Box& bx,
 const int nc,
@@ -716,12 +433,34 @@ const Real* h)
     //box extends:
     const int *lo = bx.loVect();
     const int *hi = bx.hiVect();
-	
-    typedef Kokkos::Experimental::MDRangePolicy<Kokkos::Experimental::Rank<4,Kokkos::Experimental::Iterate::Right,Kokkos::Experimental::Iterate::Right>> t_policy;
-    // Create a functor
-    C_ADOTX_Functor adotx_functor(alpha,beta,&y,&x,&a,&bX,&bY,&bZ,h);
-    // Execute functor
-    Kokkos::Experimental::md_parallel_for(t_policy({0,lo[2],lo[1],lo[0]},{nc,hi[2]+1,hi[1]+1,hi[0]+1},{1,4,4,1024000}),adotx_functor);
+    const int *cb = bx.cbVect();
+
+    //some parameters                                                                                                                                                                                           
+    Real dhx = beta/(h[0]*h[0]);
+    Real dhy = beta/(h[1]*h[1]);
+    Real dhz = beta/(h[2]*h[2]);
+
+    //create views
+    ViewFab<Real> yv(y,"y"), xv(x,"x"), av(a,"a"), bXv(bX,"bX"), bYv(bY,"bY"), bZv(bZ,"bZ");
+
+    //create policy
+    typedef Kokkos::Experimental::MDRangePolicy<Kokkos::Experimental::Rank<3> > t_policy;
+    
+    //execute
+    for(int n=0; n<nc; n++){
+      Kokkos::Experimental::md_parallel_for(t_policy({lo[3],lo[2],lo[1]},{hi[3]+1,hi[2]+1,hi[1]+1},{cb[3],cb[2],cb[1]}),
+					    [&](const int& k, const int& j, const int& i){
+					      yv(i,j,k,n) = alpha * av(i,j,k) * xv(i,j,k,n)
+						- dhx * (   bXv(i+1,j,  k  ) * ( xv(i+1,j,  k,  n) - xv(i,  j,  k  ,n) )
+							  - bXv(i,  j,  k  ) * ( xv(i,  j,  k,  n) - xv(i-1,j,  k  ,n) ) )
+						- dhy * (   bYv(i,  j+1,k  ) * ( xv(i,  j+1,k,  n) - xv(i,  j  ,k  ,n) )
+							  - bYv(i,  j,  k  ) * ( xv(i,  j,  k,  n) - xv(i,  j-1,k  ,n) ) )
+						- dhz * (   bZv(i,  j,  k+1) * ( xv(i,  j,  k+1,n) - xv(i,  j  ,k  ,n) )
+	      					          - bZv(i,  j,  k  ) * ( xv(i,  j,  k,  n) - xv(i,  j,  k-1,n) ) );
+					    });
+    }
+    yv.fill(y);
+
 }
 
 //-----------------------------------------------------------------------
